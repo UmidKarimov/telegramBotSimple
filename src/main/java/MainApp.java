59dbd7a3d887319c50c078e8dab733cb1283b32b
Umid Karimov <Umid.Karimov@unibank.az>
2022-02-10 13:16:46 +0400

    Initial commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,17 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new UmidBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/UmidBot.java
@@ -0,0 +1,62 @@
+import org.telegram.telegrambots.api.methods.send.SendMediaGroup;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.User;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class UmidBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public String getBotUsername() {
+        return "umid_test_1_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5168171028:AAHkn6NFytOCEhXGSKb8UCPhcEh6DKCnhdk";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String command = update.getMessage().getText();
+        User user = update.getMessage().getFrom();
+        SendMessage sendMessage = new SendMessage();
+
+        if (command.equals("/myname")) {
+            sendMessage.setText(user.getFirstName());
+        } else if (command.equals("/mylastname")) {
+            sendMessage.setText(user.getLastName());
+        } else if (command.equals("/myfullname")) {
+            sendMessage.setText(user.getLastName() + " " + user.getFirstName());
+        } else if (command.equals("/time")) {
+            sendMessage.setText(getTime());
+        } else if (command.equals("/date")) {
+            sendMessage.setText(getDate());
+        } else if (command.equals("/datetime")) {
+            sendMessage.setText(getDate() + " " + getTime());
+        } else {
+            sendMessage.setText("Unrecognized command");
+        }
+
+        sendMessage.setChatId(update.getMessage().getChatId());
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getDate() {
+        return new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+    }
+
+    private String getTime() {
+        return new SimpleDateFormat("HH:mm:ss").format(new Date());
+    }
+}
